01b84b37b52fd03b0d795fbf4bdf02f311073ab7
aronaNg <arona.ngom07@gmail.com>
2023-11-16 09:19:47 +0100

    add types model

--- app/Models/TypeEquipement.php
+++ app/Models/TypeEquipement.php
@@ -8,4 +8,6 @@ use Illuminate\Database\Eloquent\Model;
 class TypeEquipement extends Model
 {
     use HasFactory;
+    protected $fillable = ['intitule'];
+
 }
--- app/Models/TypeIncident.php
+++ app/Models/TypeIncident.php
@@ -8,4 +8,6 @@ use Illuminate\Database\Eloquent\Model;
 class TypeIncident extends Model
 {
     use HasFactory;
+    protected $fillable = ['intitule'];
+
 }
