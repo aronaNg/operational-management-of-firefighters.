c76d9348663caebc402f52520b62fa7b0908ad32
aronaNg <arona.ngom07@gmail.com>
2023-11-16 08:26:27 +0100

    add authentication and bootstrap...

--- routes/web.php
+++ routes/web.php
@@ -16,7 +16,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
-});
+})->name("accueil");
 
 Route::get('/dashboard', function () {
     return view('dashboard');
@@ -29,3 +29,7 @@ Route::middleware('auth')->group(function () {
 });
 
 require __DIR__.'/auth.php';
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
