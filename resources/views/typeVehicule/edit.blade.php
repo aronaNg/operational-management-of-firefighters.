de9794c98cf11f3561005b9f38b675ec2b64e40f
aronaNg <arona.ngom07@gmail.com>
2023-11-19 19:04:56 +0100

    add table and pagination

--- resources/views/typeVehicule/edit.blade.php
+++ resources/views/typeVehicule/edit.blade.php
@@ -1,35 +1,37 @@
 @extends("layouts.app")
 
 @section("content")
-<div class="my-3 p-3 bg-body rounded shadow-sm">
-    <h3 class="border-bottom pb-2 mb-4">Édition d'un ype véhicule</h3>
+<div class="container">
+    <div class="my-3 p-3 bg-body rounded shadow-sm">
+        <h3 class="border-bottom pb-2 mb-4">Édition d'un type véhicule</h3>
 
-    <div class="mt-4">
+        <div class="mt-4">
 
-        @if ($errors->any())
-            <div class="alert alert-danger alert-dismissible fade show" role="alert"">
-                <ul>
-                    @foreach ($errors->all() as $error)
-                        <li>{{ $error }}</li>
-                    @endforeach
-                </ul>
-                <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
-            </div>
-        @endif
-        <form method="POST" action="{{route('admin.update', ['typeVehicule'=>$typeVehicule->id])}}">
-            @csrf
+            @if ($errors->any())
+                <div class="alert alert-danger alert-dismissible fade show" role="alert"">
+                    <ul>
+                        @foreach ($errors->all() as $error)
+                            <li>{{ $error }}</li>
+                        @endforeach
+                    </ul>
+                    <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+                </div>
+            @endif
+            <form method="POST" action="{{route('admin.update', ['typeVehicule'=>$typeVehicule->id])}}">
+                @csrf
 
-            <input type="hidden" name="_method" value="put"/>
+                <input type="hidden" name="_method" value="put"/>
 
-            <div class="mb-3">
-              <label for="intitule" class="form-label">Nom du type vehicule</label>
-              <input type="text" class="form-control" id="intitule" name="intitule" value="{{$typeVehicule->intitule}}">
-            </div>
+                <div class="mb-3">
+                  <label for="intitule" class="form-label">Nom du type vehicule</label>
+                  <input type="text" class="form-control" id="intitule" name="intitule" value="{{$typeVehicule->intitule}}">
+                </div>
 
-            <button type="submit" class="btn btn-primary">Enregistrer</button>
-            <a href="{{route('admin')}}" class="btn btn-danger">Annuler</a>
+                <button type="submit" class="btn btn-primary">Enregistrer</button>
+                <a href="{{route('admin')}}" class="btn btn-danger">Annuler</a>
 
-          </form>
-    </div>
+              </form>
+        </div>
+</div>
 
 @endsection
