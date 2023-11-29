e5ec9bc5797fbcaf48f7d3a06692ab0a1721ed80
aronaNg <arona.ngom07@gmail.com>
2023-11-29 15:14:17 +0100

    seeding databases and stop project...

--- requetes.sql
+++ requetes.sql
@@ -27,14 +27,31 @@
 
 -- historisation pour la table type equipement
 
-DELIMITER $$
+-- DELIMITER $$
 
-CREATE TRIGGER trig_archiveequip
-AFTER DELETE ON type_equipements
-FOR EACH ROW
-BEGIN
-    INSERT INTO table_historiqueequip (id, intitule)
-    VALUES (OLD.id, OLD.intitule);
-END$$
+-- CREATE TRIGGER trig_archiveequip
+-- AFTER DELETE ON type_equipements
+-- FOR EACH ROW
+-- BEGIN
+--     INSERT INTO table_historiqueequip (id, intitule)
+--     VALUES (OLD.id, OLD.intitule);
+-- END$$
 
-DELIMITER ;
+-- DELIMITER ;
+
+-- interventions
+-- DELIMITER //
+
+-- CREATE EVENT IF NOT EXISTS insert_event
+-- ON SCHEDULE EVERY 1 MINUTE
+-- DO
+--   BEGIN
+--     INSERT INTO intervention (id, statut, date_heure, id_type_incident, id_pompier, id_vehicule, id_equipement)
+--     VALUES (NULL, 'En cours', NOW(),
+--             FLOOR(1 + RAND() * 10),
+--             FLOOR(1 + RAND() * 10),
+--             FLOOR(1 + RAND() * 10),
+--             FLOOR(1 + RAND() * 10));
+--   END //
+
+-- DELIMITER ;
